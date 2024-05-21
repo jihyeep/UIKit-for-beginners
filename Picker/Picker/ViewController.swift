b9dd4d0abcd05857c35d09215147256d0b8b27ff
Jihye Park <deviloper.park@gmail.com>
2024-05-21 14:45:36 +0900

    UIColorPickerView

--- Picker/Picker/ViewController.swift
+++ Picker/Picker/ViewController.swift
@@ -26,6 +26,11 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     let options = ["Option 1", "Option 2", "Option 3", "Option 4", "Option 5"]
     let options2 = ["옵션 1", "옵션 2", "옵션 3", "옵션 4", "옵션 5"]
     
+    // iOS에 자원이 부족하면 호출되는 함수
+    override func didReceiveMemoryWarning() {
+
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -36,6 +41,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
 //            pickerView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
 //        ])
         
+        // datePicker
         let today = Date()
         var dateComponents = DateComponents()
         dateComponents.year = -1
@@ -52,9 +58,31 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
             
         view.addSubview(datePicker)
         
+//        NSLayoutConstraint.activate([
+//            datePicker.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//            datePicker.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+//        ])
+        
+        // colorPicker
+        let button = UIButton()
+        var config = UIButton.Configuration.filled()
+        config.title = "Color Picker"
+        config.cornerStyle = .capsule
+        button.configuration = config
+        
+        button.addAction(UIAction { [weak self] _ in
+            let colorPicker = UIColorPickerViewController()
+            colorPicker.delegate = self
+            colorPicker.supportsAlpha = false // 투명도 지원 X
+            colorPicker.selectedColor = self?.view.backgroundColor ?? .white
+            self?.present(colorPicker, animated: true)
+        }, for: .touchUpInside)
+        
+        self.view.addSubview(button)
+
         NSLayoutConstraint.activate([
-            datePicker.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            datePicker.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor)
         ])
         
     }
@@ -97,6 +125,15 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         }
     }
     
-    
 }
 
+// MARK: - UIColorPickerViewControllerDelegate
+extension ViewController: UIColorPickerViewControllerDelegate {
+    func colorPickerViewController(_ viewController: UIColorPickerViewController, didSelect color: UIColor, continuously: Bool) {
+        view.backgroundColor = viewController.selectedColor
+    }
+
+    func colorPickerViewControllerDidFinish(_ viewController: UIColorPickerViewController) {
+        dismiss(animated: true)
+    }
+}
