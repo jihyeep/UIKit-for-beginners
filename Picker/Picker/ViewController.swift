594d1e9ccd937862543e7e1aeba516c9ee25bcaf
Jihye Park <deviloper.park@gmail.com>
2024-05-21 13:15:53 +0900

    UIPickerView Delegate & DataSource

---
+++ Picker/Picker/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Picker
+//
+//  Created by 박지혜 on 5/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    private lazy var pickerView: UIPickerView = {
+        let pickerView = UIPickerView()
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        pickerView.translatesAutoresizingMaskIntoConstraints = false
+        return pickerView
+    }()
+    
+    let options = ["Option 1", "Option 2", "Option 3", "Option 4", "Option 5"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(pickerView)
+        
+        NSLayoutConstraint.activate([
+            pickerView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            pickerView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
+    // MARK: - UIPickerViewDelegate
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return options[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print("selected row: \(row)")
+    }
+    
+    // MARK: - UIPickerViewDataSource
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        options.count
+    }
+    
+    
+}
+
