07e600c2e88809861adf2cc1b5518304de7ef4e1
Jihye Park <deviloper.park@gmail.com>
2024-05-21 09:45:25 +0900

    UIButton with configuration

--- SegmentedControl/SegmentedControl/ViewController.swift
+++ SegmentedControl/SegmentedControl/ViewController.swift
@@ -13,8 +13,27 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let button = UIButton(type: .system) // 기본형
-        button.setTitle("Click me", for: .normal) // 눌리지 않은 상태
+//        let button = UIButton(type: .system) // 기본형
+        
+        let button = UIButton(type: .custom) // 커스텀
+        // config를 써주면 아래 코드는 쓰지 않음 -> 버튼에 직접 적용
+//        button.setTitleColor(.white, for: .normal)
+//        button.backgroundColor = .systemBlue
+//        button.layer.cornerRadius = 10
+//        button.layer.borderWidth = 2
+//        button.layer.borderColor = UIColor.white.cgColor
+        
+        // configuration에 스타일을 적용하여 버튼에 반영시킴
+        var config = UIButton.Configuration.filled()
+        
+        config.title = "Click me"
+        config.baseBackgroundColor = .systemBlue
+        config.baseForegroundColor = .white
+        config.cornerStyle = .capsule
+        config.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20) // 패딩
+        button.configuration = config
+        
+//        button.setTitle("Click me", for: .normal) // 눌리지 않은 상태
         button.addAction(UIAction { [weak self] _ in
             self?.count += 1
             print("Button was tapped! \(self?.count ?? 0)")
