b9e885c2f5ff05ed5a59a26cea94360562d0720c
Jihye Park <deviloper.park@gmail.com>
2024-05-22 09:37:44 +0900

    UILongPressGestureRecognizer

--- Gesture/Gesture/ViewController.swift
+++ Gesture/Gesture/ViewController.swift
@@ -19,9 +19,16 @@ class ViewController: UIViewController {
         
         view.addSubview(rectangle)
         
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
-        tapGesture.numberOfTapsRequired = 2 // 탭을 2번 해야 함
-        rectangle.addGestureRecognizer(tapGesture) // 탭 2번 하면 배경색이 변함
+        // Tap
+//        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+//        tapGesture.numberOfTapsRequired = 2 // 탭을 2번 해야 함
+//        rectangle.addGestureRecognizer(tapGesture) // 탭 2번 하면 배경색이 변함
+        
+        // LongPress
+        let longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress))
+        longPressGesture.minimumPressDuration = 2.0
+        longPressGesture.view?.addGestureRecognizer(longPressGesture)
+        
     }
     
     @objc func handleTap(_ sender: UITapGestureRecognizer) {
@@ -29,7 +36,12 @@ class ViewController: UIViewController {
             view.backgroundColor = (view.backgroundColor == .yellow) ? .red : .yellow
         }
     }
-
+    
+    @objc func handleLongPress(_ sender: UILongPressGestureRecognizer) {
+        if let view = sender.view, sender.state == .began {
+            view.backgroundColor = (view.backgroundColor == .green) ? .red : .green
+        }
+    }
 
 }
 
