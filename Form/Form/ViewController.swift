25a080fb68f7efa43e786b46eec34f0899dcc1e9
Jihye Park <deviloper.park@gmail.com>
2024-05-23 11:55:46 +0900

    Add TableView with section & Header & Footer

--- Form/Form/ViewController.swift
+++ Form/Form/ViewController.swift
@@ -7,7 +7,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+ 
+    lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: view.bounds, style: .insetGrouped)
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+    
     let formOneLabel = UILabel()
     let formOneTextField = UITextField()
     let formTwoLabel = UILabel()
@@ -20,10 +28,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        view.addSubview(tableView)
         
-        setupFormOne()
-        setupFormTwo()
-        setupResults()
+//        setupFormOne()
+//        setupFormTwo()
+//        setupResults()
         
         // TextField 핸들링 방법 1
         //        formOneTextField.addAction(UIAction { [weak self] _ in
@@ -38,6 +47,7 @@ class ViewController: UIViewController {
 //        formTwoTextField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
         
     }
+    
     // TextField 핸들링 방법 3(액션 개체 생성) - 강한 참조가 일어날 수 있기에 메모리 해제가 필요함
     override func viewIsAppearing(_ animated: Bool) {
         formOneTextField.addAction(textFieldAction, for: .editingChanged)
@@ -47,6 +57,41 @@ class ViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         formOneTextField.removeAction(textFieldAction, for: .editingChanged)
         formTwoTextField.removeAction(textFieldAction, for: .editingChanged)
+    }
+    
+    // MARK: - UITableViewDataSource
+    func numberOfSections(in tableView: UITableView) -> Int {
+        3
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        2
+    }
+    
+    // Section 이름 설정
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 2 {
+            return "결과"
+        } else {
+            return nil
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "폼 #1"
+        case 1:
+            return "폼 #2"
+        default:
+            return nil
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        return cell
     }
     
     func setupFormOne() {
@@ -121,5 +166,6 @@ class ViewController: UIViewController {
             resultLabelTwo.text = "폼 #2 = \(textField.text ?? "")"
         }
     }
+    
 }
 
