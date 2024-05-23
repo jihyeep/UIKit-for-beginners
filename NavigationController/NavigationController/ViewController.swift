44d428f7948a125d7630fd3e84206210ff53aba1
Jihye Park <deviloper.park@gmail.com>
2024-05-23 17:03:43 +0900

    페이지 전환 추가

--- NavigationController/NavigationController/ViewController.swift
+++ NavigationController/NavigationController/ViewController.swift
@@ -17,8 +17,8 @@ class ViewController: UIViewController {
         
         self.navigationController?.navigationBar.barStyle = .default
         self.navigationController?.navigationBar.isTranslucent = true // 투명도 설정
-//        self.navigationController?.navigationBar.backgroundColor = .systemBrown
-        self.navigationController?.navigationBar.scrollEdgeAppearance?.backgroundColor = .systemBrown
+        self.navigationController?.navigationBar.backgroundColor = .systemBrown
+//        self.navigationController?.navigationBar.scrollEdgeAppearance?.backgroundColor = .systemBrown
         self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
         
         let leftButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(leftButtonTapped))
@@ -29,6 +29,16 @@ class ViewController: UIViewController {
         let extraButton = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(extraButtonTapped))
         
         self.navigationItem.rightBarButtonItems = [rightButton, extraButton]
+        
+        let button = UIButton(type: .system)
+        button.setTitle("다음 화면으로", for: .normal)
+        button.addAction(UIAction { [weak self] _ in
+            let nextViewController =  UIViewController()
+            nextViewController.view.backgroundColor = .white
+            self?.show(nextViewController, sender: nil)
+        }, for: .touchUpInside)
+        button.frame = CGRect(x: 100, y: 100, width: 200, height: 50)
+        view.addSubview(button)
     }
     
     @objc func leftButtonTapped() {
